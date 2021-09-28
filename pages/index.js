031159d12b2cf0322c64af30b0af850a04c8b3d7
XandeFds <alexandreferrira063@gmail.com>
2021-09-28 11:03:42 -0300

    new pag

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from 'next/link'
 function home (){
-    return <div> home </div>
+    return <div> 
+        <h1>home </h1>
+        <Link>
+        <a href=" /sobre"> acessar pagina </a>
+        </Link>
+       
+      
+
+    </div>
 }
 
 export default home 
---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+function sobre (){
+    return <div> 
+        <h1>sobre </h1>
+      
+        <Link>
+        <a href=" /"> acessar pagina </a>
+        </Link>
+
+    </div>
+}
+
+export default sobre
