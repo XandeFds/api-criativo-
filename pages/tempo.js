584f5a6822b4f24f54d1780f57fe43e0d0b22aa0
XandeFds <alexandreferrira063@gmail.com>
2021-09-28 11:27:08 -0300

    new pag

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,6 +1,6 @@
 function tempo(){
     const dycamicDate = new Date ();
-    const dynamicDateString = dycamicDate.toGNTString();
+    const dynamicDateString = dycamicDate.toGMTString();
     <div>
         {dynamicDateString} (dinâmcio)
     </div>
